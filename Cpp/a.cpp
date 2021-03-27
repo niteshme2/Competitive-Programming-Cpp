4fa0349e2748f8f1b2e3a7f4893b903513e8a9d6
niteshme2 <niteshkumarme2@gmail.com>
2021-03-27 20:53:59 +0530

    asdf

--- Cpp/a.cpp
+++ Cpp/a.cpp
@@ -1,89 +1,72 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 
-void solve()
-{
-    int n,x,y,cnt=0,ans=0;
-    string s;
-    cin>>x>>y>>s;
-    int i,j;
-    n=s.length();
-    for(i=0;i<n;i++)
-    {
-        if(s[i]=='?')
-            cnt++;
-    }
-    if(cnt==n || cnt==n-1)
-    {
-        cout<<"0";
-        return;
-    }
-    int k;
-    for(k=0;k<n-1;k++)
-    {
-        if(s[k]!='?')
-            break;
-    }
-    if(k==0)
-        k++;
-    i=k;
-    while(i<n-1)
-    {
-        if(s[i]=='?')
-        {
-            if(s[i-1]!=s[i+1] && s[i+1]!='?')
-            {
-                if(s[i-1]=='J')
-                    ans+=y;
-                else
-                    ans+=x;
-            }
-            else if(s[i-1]!=s[i+1] && s[i+1]=='?')
-            {
-                j=i+2;
-                while(s[j]=='?' && j<n)
-                {
-                    j++;
-                }
-                if(j==n)
-                {
-                    break;
-                }
-                if(s[i-1]!=s[j])
-                {
-                    if(s[i-1]=='J')
-                        ans+=y;
-                    else
-                        ans+=x;
-                }
-                i=j;
-            }
-        }
-        i++;
-    }
-    for(i=0;i<n-1;i++)
-    {
-        if(s[i]=='J' && s[i+1]=='C')
-            ans+=y;
-        else if(s[i]=='C' && s[i+1]=='J')
-            ans+=x;
-    }
-    cout<<ans;
-
-
-}
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    ll t;
-    cin>>t;
-    for(int j=1;j<=t; j++)
-    {
-        cout<<"Case #"<<j<<": ";
-        solve();
-        cout<<endl;
+    ll t,n,q;
+    cin>>t>>n>>q;
+    while(t-- !=0){
+    vector<ll> a;
+    
+    cout<<"1 2 3"<<endl;
+    
+    ll size=3;
+    ll p;
+    cin>>p;
+    if(p==2){
+        a.push_back(1);
+        a.push_back(2);
+        a.push_back(3);
+        
     }
+    else if(p==3){
+        a.push_back(1);
+        a.push_back(3);
+        a.push_back(2);
+    }
+    else{
+        a.push_back(2);
+        a.push_back(1);
+        a.push_back(3);
+    }
+    for(ll i=4;i<=(n);i++){
+        ll u=0;
+        ll v=size-1;
+        while(u<v){
+            ll mid=u+((v-u)/2);
+            cout<<a[mid]<<" "<<a[mid+1]<<" "<<(i)<<endl;
+            cin>>p;
+            if(p==a[mid]){
+                v=mid;
+            }
+            else if(p==a[mid+1]){
+                
+                u=mid+1;
+            }
+            else{
+                a.insert(a.begin()+mid+1,i);
+                size++;
+                break;
+            }
+        }
+        if(size!=i){
+            if(u==0){
+                a.insert(a.begin(),i);
+            }
+            else{
+                a.push_back(i);
+            }
+            size++;
+        }
+    }
+    for(auto zzz: a){
+        cout<<zzz<<" ";
+    }
+    cout<<endl;
+    cin>>p;
+    if(p==-1){
+        break;
+    }
+}
 }
---
+++ Cpp/codejam_4.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define f(i,m, n) for(int i=m;i<n;i++)
+#define fr(i, m, n) for(int i=m;i>n;i--)
+#define fi(i, m, n) for(int i=m;i<=n;i++)
+#define fri(i, m, n) for(int i=m;i>=n;i--)
+
+#define ll long long
+#define int long long
+#define vi vector<int>
+
+
+const int MOD = 1000000007;
+inline int add(int a, int b) { return a + b >= MOD ? a + b - MOD : a + b; }
+inline void inc(int& a, int b) { a = add(a, b); }
+inline int sub(int a, int b) { return a - b < 0 ? a - b + MOD : a - b; }
+inline void dec(int& a, int b) { a = sub(a, b); }
+
+
+
+
+
+struct compare { 
+    bool operator()(const pair<int, int>& value,  
+                                const int& key) 
+    { 
+        return (value.first < key); 
+    } 
+    bool operator()(const int& key,  
+                    const pair<int, int>& value) 
+    { 
+        return (key < value.first); 
+    } 
+}; 
+
+
+
+
+
+
+
+
+
+
+void solve(){
+    int n, q;
+    cin>>n>>q;
+
+    vi arr;
+
+    int s = 3, med;
+    cin>>med;
+
+    if(med==2){
+        arr.push_back(1);
+        arr.push_back(2);
+        arr.push_back(3);
+    }
+    if(med==3){
+        arr.push_back(1);
+        arr.push_back(3);
+        arr.push_back(2);
+    }
+    if(med==1){
+        arr.push_back(2);
+        arr.push_back(1);
+        arr.push_back(3);
+    }
+    f(i, 4, n){
+        int beg = 0;
+        int end = s - 1;
+        while(beg<end){
+            ll mid=beg+((end-beg)/2);
+            cout<<arr[mid]<<" "<<arr[mid+1]<<" "<<(i)<<endl;
+            cin>>med;
+            if(med==arr[mid]){
+                end=mid;
+            }
+            else if(med==arr[mid+1]){
+                
+                beg=mid+1;
+            }
+            else{
+                arr.insert(arr.begin()+mid+1,i);
+                s++;
+                break;
+            }
+        }
+
+        if(s!=i){
+            if(beg==0) arr.insert(arr.begin(),i);
+            else arr.push_back(i);
+            s++;
+        }
+    }
+    f(i,0,arr.size()) cout<<arr[i]<<" "; cout<<endl;
+    cin>>med;
+    if(med==-1) return;
+    
+}
+
+int32_t main(){
+ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        clock_t z = clock();
+        solve();
+        debug("Total Time: %.3f\n", (double)(clock() - z) / CLOCKS_PER_SEC);
+    }    
+}
